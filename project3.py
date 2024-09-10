be5cac55cfa65e737260dedc18c332ff5501fa44
Panos <tiktapanidisp@gmail.com>
2024-09-10 23:54:44 +0300

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+OPERATORS = ["+" , "-" , "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
+
+wrong = 0
+input("Press enter to start!")
+print("--------------------------")
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + " :" + expr + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time)
+
+print("---------------------")
+print("Good Job. You finished in" , total_time , "seconds!")
+
+
+expr, answer = generate_problem()
+print(expr,answer)
